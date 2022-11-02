b87a81549636ee1e239132ed94a797881ffa2402
Wsko-12 <vaskovladislav@gmail.com>
2022-11-02 15:07:19 +0300

    feat: add 2 part (animation)

---
+++ v2/2. animation/start/utils/canvasUtils.js
@@ -0,0 +1,8 @@
+export const drawCircle = (ctx, x, y, radius, color = 'orange') => {
+    ctx.save();
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.restore();
+}
